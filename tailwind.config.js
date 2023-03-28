477c7f488538548e81d2dfae71c4c5588d0f7100
Sneh Mehta <smehta@colum.edu>
2023-03-28 04:22:39 +0000

    feat: added tailwind config file from previous codebase

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,33 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGreen: {
+          300: "#010A0A",
+          500: "#43A375",
+          700: "#02706F",
+          900: "#013C3C",
+        },
+        lightGreen: {
+          50: "#E2F0EC",
+          100: "#98DDBB",
+          300: "#548A70",
+          500: "#43A375",
+          700: "rgba(84, 198, 142, 0.6)",
+        },
+        neutrals: {
+          100: "#F6F6F6",
+          200: "#E2E8F0",
+          300: "#CBD5E1",
+          400: "#94A3B8",
+          500: "#64748B",
+          600: "#475569",
+          700: "#1E29EB",
+          800: "#0F172A",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
