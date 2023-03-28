ad406aca7da856b14e837152dd9783c799846514
Rafael Santana <rsantana@colum.edu>
2023-03-27 23:25:18 -0500

    added boilerplate

---
+++ components/cards.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "cards"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/mainpage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "mainpage"
+}
+</script>
+
+<style scoped>
+
+</style>
