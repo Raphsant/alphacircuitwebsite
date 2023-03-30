eb5b42313a9fb18745ac102888ad688c7b3ca7ab
Sneh Mehta <smehta@colum.edu>
2023-03-30 07:03:04 +0000

    feat: added in the header as part of the default layout - added images and tailwind config color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,37 @@ module.exports = {
     "./nuxt.config.{js,ts}",
     "./app.vue"
   ],  theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGreen: {
+          300: "#010A0A",
+          500: "#43A375",
+          700: "#02706F",
+          900: "#013C3C",
+        },
+        lightGreen: {
+          50: "#E2F0EC",
+          100: "#98DDBB",
+          300: "#548A70",
+          500: "#43A375",
+          700: "rgba(84, 198, 142, 0.6)",
+        },
+        neutrals: {
+          100: "#F6F6F6",
+          200: "#E2E8F0",
+          300: "#CBD5E1",
+          400: "#94A3B8",
+          500: "#64748B",
+          600: "#475569",
+          700: "#1E29EB",
+          800: "#0F172A",
+        },
+      },
+      // fontFamily: {
+      //   manrope: "'Manrope', sans-serif",
+      //   ubuntu: "'Ubuntu', sans-serif",
+      // },
+    },
   },
   plugins: [],
 }
