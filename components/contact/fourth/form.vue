b200d3d5f4f6f33ce4d208009dacbdcc11c64641
Raph Santana <raphy@raphsant.me>
2023-04-23 14:36:27 -0500

    added button to form

--- components/contact/fourth/form.vue
+++ components/contact/fourth/form.vue
@@ -4,11 +4,12 @@
         Send us a message! Get in touch with one of the team today!
     </div>
     <div>
-        <div class="flex flex-col gap-5">
-            <input type="text" class="w-1/5 rounded-md h-10">
-            <input type="text" class="w-1/5 rounded-md h-10">
-            <input type="text" class="w-1/5 rounded-md h-10">
-            <input type="text" class="w-2/6 h-36 rounded-md ">
+        <div class="flex flex-col gap-5 justify-start">
+            <input type="text" placeholder="Name" class="w-1/5 rounded-md h-10 p-2">
+            <input type="text" placeholder="Subject" class="w-1/5 rounded-md h-10 p-2">
+            <input type="text" placeholder="Email" class="w-1/5 rounded-md h-10 p-2">
+            <textarea placeholder="Your requirement" class="w-2/6 h-36 rounded-md p-2 pb-20"/>
+            <button class="bg-darkGreen-900 rounded-md p-3 w-1/6 text-white font-bold text-lg">Send</button>
         </div>
     </div>
 </div>
