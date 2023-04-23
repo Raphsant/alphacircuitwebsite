fa43feaaaf7e47d7d61fb62846b0ebbce1bd1fe5
Raph Santana <raphy@raphsant.me>
2023-04-23 16:16:48 -0500

    replaced markup with conditional rendering

---
+++ components/index/third/indexcards.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="bg-lightGreen-50 flex flex-col justify-center items-center w-2/5 rounded-lg drop-shadow-lg p-3">
+    <div>
+        <img :src="url" alt="svg">
+    </div>
+    <div>
+        <div>
+            {{props.text}}
+        </div>
+    </div>
+
+</div>
+</template>
+
+<script setup>
+
+const props = defineProps(['imageUrl', 'text'])
+const url = `/img/${props.imageUrl}`
+
+</script>
+
+<style scoped>
+
+</style>
