dabb53dcba348cc167538b21ea3825994ad48954
Rafael Santana <rsantana@colum.edu>
2023-05-28 14:53:38 -0500

    feat: added the icon images for the ContactCard.vue

--- components/contact/fourth/ContactCard.vue
+++ components/contact/fourth/ContactCard.vue
@@ -1,19 +1,31 @@
 <template>
-    <div class="flex-col flex border bg-neutral-50 rounded-lg w-3/12 space-y-4 p-5">
-        <img :src="props.imgUrl" alt="">
-        <div class="text-3xl w-fit">
-            {{props.title}}
-        </div>
-        <div class="w-3/6 text-lg">
-            {{props.body}}
-        </div>
-        <div class="bg-darkGreen-700 text-white w-fit p-3 font-bold rounded-lg cursor-pointer hover:underline">
-            {{props.buttonText}}
-        </div>
+  <div
+    class="flex-col flex border bg-neutral-50 rounded-lg w-3/12 space-y-4 p-5"
+  >
+    <img class="w-16" :src="props.imgUrl" alt="" />
+    <div class="text-3xl w-fit">
+      {{ props.title }}
     </div>
+    <div class="w-3/6 text-lg">
+      {{ props.body }}
+    </div>
+    <div
+      class="
+        bg-darkGreen-700
+        text-white
+        w-fit
+        p-3
+        font-bold
+        rounded-lg
+        cursor-pointer
+        hover:underline
+      "
+    >
+      {{ props.buttonText }}
+    </div>
+  </div>
 </template>
 <script setup>
-const props = defineProps(['title', 'imgUrl', 'body', 'buttonText'])
+const props = defineProps(["title", "imgUrl", "body", "buttonText"]);
 </script>
-<style scoped>
-</style>
+<style scoped></style>
