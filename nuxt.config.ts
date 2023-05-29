1c8b51f110cd0e3f2650ccea6d3c7b20fe453e52
Rafael Santana <rsantana@colum.edu>
2023-05-29 00:21:36 -0500

    feat: <meta> added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Alpha Circuit",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1.0",
+      keywords:
+        "Quick turn prototype,PWB Fabricator,Quick turn production,TS16949,AS9100,Automotive,Mil-31032,PCB Reliability",
+      description: "Prototype Design",
+    },
+  },
   modules: [
     [
       "nuxt-mail",
