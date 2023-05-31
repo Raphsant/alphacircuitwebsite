ceee7cfe302d20f67a730058e3923154a427d8da
Rafael Santana <rsantana@colum.edu>
2023-05-31 12:55:48 -0500

    feat: finished edits for contact.vue and ContactCard.vue pages.

--- components/contact/fourth/ContactCard.vue
+++ components/contact/fourth/ContactCard.vue
@@ -1,18 +1,51 @@
 <template>
-    <div class="flex-col flex border bg-neutral-50 rounded-lg w-3/12 space-y-4 p-5 shadow-xl">
-        <img class="w-16" :src="props.imgUrl" alt="">
-        <div class="text-3xl w-fit">
-            {{props.title}}
-        </div>
-        <div class="w-4/6 text-lg">
-            {{props.body}}
-        </div>
-        <div class="bg-darkGreen-700 text-white w-fit p-3 font-bold rounded-lg cursor-pointer hover:underline">
-            {{props.buttonText}}
-        </div>
+  <div
+    class="
+      flex-col flex
+      border
+      bg-neutral-50
+      rounded-lg
+      w-3/12
+      space-y-4
+      p-5
+      shadow-xl
+    "
+  >
+    <img class="w-16" :src="props.imgUrl" alt="" />
+    <div class="text-3xl w-fit">
+      {{ props.title }}
     </div>
+    <div class="w-4/6 text-lg">
+      {{ props.body }}
+    </div>
+    <NuxtLink :to="props.hash ? fullLink : props.redirectLink">
+      <div
+        class="
+          bg-darkGreen-700
+          text-white
+          w-fit
+          p-3
+          font-bold
+          rounded-lg
+          cursor-pointer
+          hover:underline
+        "
+      >
+        {{ props.buttonText }}
+      </div>
+    </NuxtLink>
+  </div>
 </template>
 <script setup>
-const props = defineProps(["title", "imgUrl", "body", "buttonText"]);
+const props = defineProps([
+  "title",
+  "imgUrl",
+  "body",
+  "buttonText",
+  "redirectLink",
+  "hash",
+]);
+
+const fullLink = props.redirectLink + props.hash;
 </script>
 <style scoped></style>
