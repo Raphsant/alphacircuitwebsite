74c11f581ca60ef5210e815d6c764be73a5f0607
Sneh Mehta <smehta@colum.edu>
2023-06-01 21:10:39 -0500

    feat: capabilities page is done, needs polishing perhaps

--- pages/capabilities.vue
+++ pages/capabilities.vue
@@ -1,5 +1,322 @@
 <template>
-    
+    <section class="w-10/12 mx-auto mt-12">
+        <div>
+            <div class="flex justify-end">
+                <!-- <div class="flex flex-col">
+                    <h3 class="text-2xl">Capabilities</h3>
+                    <hr class="w-22 h-1 bg-darkGreen-700 border-0 rounded dark:bg-gray-700"/>
+                </div> -->
+                <div class="inline-flex mb-8 items-center justify-center w-full">
+    <hr class="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+    <span class="absolute px-3 font-medium text-darkGreen-700 text-2xl -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">Capabilities</span>
+</div>
+            </div>
+        </div>
+        <div class="space-y-2 mb-12">
+            <h2 class="text-lg font-bold font-ubuntu">ALPHA IS: RIGID + RIGID-FLEX + FLEX + IMS DIGITAL / RF / MIXED PCBs</h2>
+            <p class="font-manrope">Alpha's technical capabilities and extensive experience in high end technology PCBs in the RF, non-RF and mixed PCBs are among the industry leaders. Our growth is also focused on thermal management which is becoming increasingly important for the emerging electronic devices in telecommunication and hybrid devices, HDI and building circuits for the emerging markets in automotives, displays, HDI, telecommunication, medical, defense, etc. <span class="font-bold">Below is a short list of our technical capabilities</span></p>
+        </div>
+
+        <!-- Table 1 -->
+        <div class="w-10/12 mx-auto bg-darkGreen-700 p-8 rounded-lg">
+            <table class="bg-neutrals-200 font-manrope rounded-lg">
+                <thead class="">
+                    <tr class="bg-lightGreen-100">
+                        <th colspan="2" class="text-left py-1 pr-1 border-b-2 border-white rounded-tl rounded-tr">
+                            <h4>ACC Capabilities at a Glance</h4>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-b-2 border-white">
+                        <td class="bg-lightGreen-100 py-1 pr-1">Layer Count</td>
+                        <td class="p-1">1 to 30+</td>
+                    </tr>
+                    <tr class="border-b-2 border-white">
+                        <td class="bg-lightGreen-100 py-1 pr-1">Largest Panel Size</td>
+                        <td class="p-1">18" x 24"</td>
+                    </tr>
+                    <tr class="border-b-2 border-white">
+                        <td class="bg-lightGreen-100 py-1 pr-1">Smallest Circuit Width/Spacing</td>
+                        <td class="p-1">3 mil; 2 mil for prototypes</td>
+                    </tr>
+                    <tr class="border-b-2 border-white">
+                        <td class="bg-lightGreen-100 py-1 pr-1">Material Types</td>
+                        <td class="p-1">Rogers, Polyimide, PET, LPC, FR4,Metal-Core Heat Sinks</td>
+                    </tr>
+                    <tr class="border-b-2 border-white">
+                        <td class="bg-lightGreen-100 py-1 pr-1">Smallest Mech. Drilled Hole</td>
+                        <td class="p-1">.004"</td>
+                    </tr>
+                    <tr class="border-b-2 border-white">
+                        <td class="bg-lightGreen-100 py-1 pr-1">Hole Size &amp; Dim. Tolerances</td>
+                        <td class="p-1">+/- .003" and +/- .002"</td>
+                    </tr>
+                    <tr class="border-b-2 border-white">
+                        <td class="bg-lightGreen-100 py-1 pr-1">Finishes / Lead Free </td>
+                        <td class="p-1">ENIG, Immersion Silver, Immersion Tin,OSP, Lead-Free HAL, Hard TAB Gold, Wire Bondable Soft Gold</td>
+                    </tr>
+                    <tr class="border-b-2 border-white">
+                        <td class="bg-lightGreen-100 py-1 pr-1">Electrical Testing</td>
+                        <td class="p-1">Flying Probe</td>
+                    </tr>
+                    <tr class="border-b-2 border-white">
+                        <td class="bg-lightGreen-100 py-1 pr-1">Fabrication</td>
+                        <td class="p-1">Routing &amp; Scoring, Array Layouts</td>
+                    </tr>
+                    <tr class="border-b-2 border-white">
+                        <td class="bg-lightGreen-100 py-1 pr-1">Plasma Processing Unit</td>
+                        <td class="p-1">Printed Electronics with Silver Ink Bumping, Metal-Core, Heat Sink</td>
+                    </tr>
+                    <tr class="">
+                        <td class="bg-lightGreen-100 py-1 pr-1 rounded-bl">Laser</td>
+                        <td class="p-1">Drilling, Routing and Trimming</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="flex flex-row m-4 w-10/12 mx-auto justify-center space-x-10 mb-8">
+            <a class="hover:underline bg-darkGreen-700 text-white p-2 rounded-lg shadow-xl" href="">(1)Sputtered Blind Hole Technology Paper</a>
+            <a class="hover:underline bg-darkGreen-700 text-white p-2 rounded-lg shadow-xl" href="">(2)Thermal Management of RF and Digital Electronic Devices</a>
+        </div>
+
+        <div class="text-center mx-auto mb-8">
+            <h1 class="text-4xl font-ubuntu">"Our goal is to meet or exceed your expectations"</h1>
+            <p class="text-neutrals-400 font-manrope">-Prashant Patel, CEO</p>
+        </div>
+
+        <hr class="w-12/12 mx-auto h-1 bg-lightGreen-50 border-0 rounded dark:bg-gray-700 drop-shadow-xl"/>
+
+        <!-- Section with Multiple Tables -->
+        <section class="grid grid-rows-2 grid-cols-2 w-12/12 mx-auto font-manrope place-items-center mb-8">
+            <div class="space-y-2">
+                <div class="space-y-1">
+                    <p>Production Capabilities</p>
+                    <hr class="w-12 h-1 bg-darkGreen-700 border-0 rounded dark:bg-gray-700"/>
+                </div>
+                <table >
+                    <thead>
+                        <tr>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Specification</h4>
+                            </th>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Current Capability</h4>
+                            </th>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Future Capabilities</h4>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-neutrals-200">
+                        <tr class="border-b-2 border-white">
+                            <td >Substrate Minimum Thickness</td>
+                            <td >.001"</td>
+                            <td >.001"</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Max. Operating Temperature</td>
+                            <td >356F</td>
+                            <td >500F</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Maximum Layer Count</td>
+                            <td >24+</td>
+                            <td >30+</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Silver Conductive Ink Impedance</td>
+                            <td >&lt; 6 m? / Sq / Mil</td>
+                            <td >&lt; 6 m? / Sq / Mil</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="space-y-2">
+                <div class="space-y-1">
+                    <p>Fabrication</p>
+                    <hr class="w-12 h-1 bg-darkGreen-700 border-0 rounded dark:bg-gray-700"/>
+                </div>
+                <table >
+                    <thead>
+                        <tr>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Specification</h4>
+                            </th>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Current Capability</h4>
+                            </th>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Future Capabilities</h4>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-neutrals-200">
+                        <tr class="border-b-2 border-white">
+                            <td >Minimum Finished Hole Size</td>
+                            <td >.002"</td>
+                            <td >.001"</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Blind/Buried Vias</td>
+                            <td >.003" - .002"</td>
+                            <td >.001"</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Routing Tolerance</td>
+                            <td >.005"</td>
+                            <td >.005"</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Scoring Tolerance</td>
+                            <td >.002"</td>
+                            <td >.002"</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="space-y-2">
+                <div class="space-y-1">
+                    <p>Imaging</p>
+                    <hr class="w-12 h-1 bg-darkGreen-700 border-0 rounded dark:bg-gray-700"/>
+                </div>
+                <table >
+                    <thead>
+                        <tr>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Specification</h4>
+                            </th>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Current Capability</h4>
+                            </th>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Future Capabilities</h4>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-neutrals-200">
+                        <tr class="border-b-2 border-white">
+                            <td >Minimum Lines/Spaces</td>
+                            <td >.002"</td>
+                            <td >.001"</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Min. Quad Flat Pack Pitch</td>
+                            <td >.016"</td>
+                            <td >.016 - .012'</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Minimum Pad Size</td>
+                            <td >.004"</td>
+                            <td >.004" - .003"</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Solder mask Registration</td>
+                            <td >.003"</td>
+                            <td >.002"</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Solder mask Tolerance</td>
+                            <td >.002"</td>
+                            <td >.002"</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="space-y-2">
+                <div class="space-y-1">
+                    <p>Plating & Surface Finishes</p>
+                    <hr class="w-12 h-1 bg-darkGreen-700 border-0 rounded dark:bg-gray-700"/>
+                </div>
+                <table >
+                    <thead>
+                        <tr>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Specification</h4>
+                            </th>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Current Capability</h4>
+                            </th>
+                            <th class="text-left bg-lightGreen-100 py-2 pr-2 border-b-2 border-white">
+                                <h4>Future Capabilities</h4>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-neutrals-200">
+                        <tr class="border-b-2 border-white">
+                            <td >Finished Copper Thickness</td>
+                            <td >20 ounce</td>
+                            <td >20 ounce</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Hot Air Solder Leveling</td>
+                            <td >Yes</td>
+                            <td >Yes</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Lead-Free Solder (HAL)</td>
+                            <td >Yes</td>
+                            <td >Yes</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Full Body Hard Ni/Au</td>
+                            <td >Yes</td>
+                            <td >Yes</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >Immersion Gold/Silver</td>
+                            <td >Yes</td>
+                            <td >Yes</td>
+                        </tr>
+                        <tr class="border-b-2 border-white">
+                            <td >OSP</td>
+                            <td >Yes</td>
+                            <td >Yes</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+
+        <hr class="w-12/12 mx-auto h-1 bg-lightGreen-50 border-0 rounded dark:bg-gray-700 drop-shadow-xl"/>
+
+    </section>
+
+    <!-- Technology Section -->
+    <section class="w-10/12 mx-auto mt-2 mb-8 flex flex-col gap-4 font-manrope">
+
+        <div class="space-y-2">
+            <div class="space-y-1">
+                <p class="text-2xl">Technology</p>
+                <hr class="w-12 h-1 bg-darkGreen-700 border-0 rounded dark:bg-gray-700"/>
+            </div>
+            <p>As the telecommunication, big data analytics and high frequency spectrum expands, every key player in the electronics industry is keen to tap the market opportunities. Electronics device manufacturer will rely heavily on the PCB manufacturers to supply and support their systems for these emerging high technology markets. Based on the current market opportunity trend in hardware electronics, our high technology road map is focused on the following sectors.</p>
+        </div>
+
+        <div>
+            <h5 class="font-bold font-ubuntu">Industrial Automation and Mass Transit</h5>
+            <p>(1) PCBs for electronic devices in Industrial Automation and Mass Transit: Traditionally trains, buses and trucks, airplanes and ships use large mechanical devices with big motors, levers, bearings, etc. that are controlled by actuators. A lot of electronics goes into all these systems as many of the actuators are controlled electronically. Many pneumatic actuators are similarly controlled by electronics. With more complex monitoring and control that are taking place in new transit systems, we will be seeing increasingly more electronics. Ultimately, all of these technologies, in road transport, railroads, aerospace and sea transits, translate into laying out a road map for high end PCBs needed by them. Of course, autonomous driving is the end goal in some of these applications, wherein there are sort of different levels of automation. Usually level zero is when the driver is in sole control and the state of automation gradually progresses up to level five whereby the vehicle is entirely autonomous and may not even have a steering wheel.</p>
+        </div>
+
+        <div>
+            <h5 class="font-bold font-ubuntu">Medical/Pharmaceutical</h5>
+            <p>(2) Medical/Pharmaceutical devices. (a) Rigid and flexible PCBs for high density interconnects required for telecommunication. Medical professionals and surgeons will be interacting remotely with the patient and hospital staff to monitor and control actual medical procedures, diagnosis or analysis of patient's data. Cross functional teams from multiple locations worldwide will interact with each other in critical situations either for an individual patient or for a massive disaster affected zone. Solutions for addressing these situations will need reliable, high technology PCBs. Similarly, telecommunication will be required for medical implants in patients, and R&D; on genetic engineering will require remote electronic data exchange among different research teams at multiple locations. (b) PCBs for non RF applications include more ergonomic beds, chairs and other accessories directly controlled by patients, and other diagnostic tools.</p>
+        </div>
+
+        <div>
+            <h5 class="font-bold font-ubuntu">Power Electronics</h5>
+            <p>(3) Power Electronics. Advanced PCBs will be required for renewable and green energy power generation units. Examples are solar devices, lithium-ion battery, DC power supply rectifiers, automotive industry, etc. Extensive use of RF and digital PCBs are required in such installations.</p>
+        </div>
+
+        <div>
+            <h5 class="font-bold font-ubuntu">Thermal Management</h5>
+            <p>(4) Thermal Management. We have an extensive R&D; portfolio and experience in making PCBs that need effective thermal management for a variety of RF/analog, digital or hybrid circuits. We tailor the thermal material compositionally to meet specific requirements of the customer in terms of thermal, mechanical and environmental stability of the overall electronics device and module, and offer design guidelines to meet each customer's specific application.</p>
+        </div>
+    </section>
 </template>
   
 <script>
