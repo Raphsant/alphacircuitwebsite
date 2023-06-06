f4441428014f6a31111ccb24b30c02dd85265186
Rafael Santana <rsantana@colum.edu>
2023-06-06 16:28:53 -0500

    fix: netlify forms

--- components/contact/fourth/form.vue
+++ components/contact/fourth/form.vue
@@ -1,47 +1,59 @@
 <template>
-  <div class="flex flex-col gap-5 p-12">
-    <div class="font-bold text-3xl font-ubuntu">
-      Send us a message! Get in touch with one of the team today!
-    </div>
-    <div>
-      <div class="flex flex-col gap-5">
-        <input
-          type="text"
-          placeholder="Name"
-          class="font-manrope rounded-md h-10 p-2"
-        />
-        <input
-          type="text"
-          placeholder="Subject"
-          class="font-manrope rounded-md h-10 p-2"
-        />
-        <input
-          type="text"
-          placeholder="Email"
-          class="font-manrope rounded-md h-10 p-2"
-        />
-        <textarea
-          placeholder="Your requirement"
-          class="font-manrope h-36 rounded-md p-2 pb-20"
-        />
-        <button
-          @click="send"
-          class="
-            bg-darkGreen-900
-            rounded-md
-            p-3
-            text-white
-            font-bold font-manrope
-            text-lg
-            w-max
-            hover:underline
-          "
-        >
-          Send
-        </button>
+  <form
+    name="contact-us"
+    method="POST"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+  >
+    <input type="hidden" name="form-name" value="contact-us" />
+    <div class="flex flex-col gap-5 p-12">
+      <div class="font-bold text-3xl font-ubuntu">
+        Send us a message! Get in touch with one of the team today!
+      </div>
+      <div>
+        <div class="flex flex-col gap-5">
+          <input
+            type="text"
+            name="Name"
+            placeholder="Name"
+            class="font-manrope rounded-md h-10 p-2"
+          />
+          <input
+            type="text"
+            name="Subject"
+            placeholder="Subject"
+            class="font-manrope rounded-md h-10 p-2"
+          />
+          <input
+            type="text"
+            name="Email"
+            placeholder="Email"
+            class="font-manrope rounded-md h-10 p-2"
+          />
+          <textarea
+            name="Requirement"
+            placeholder="Your requirement"
+            class="font-manrope h-36 rounded-md p-2 pb-20"
+          />
+          <button
+            type="submit"
+            class="
+              bg-darkGreen-900
+              rounded-md
+              p-3
+              text-white
+              font-bold font-manrope
+              text-lg
+              w-max
+              hover:underline
+            "
+          >
+            Send
+          </button>
+        </div>
       </div>
     </div>
-  </div>
+  </form>
 </template>
 
 <script setup></script>
